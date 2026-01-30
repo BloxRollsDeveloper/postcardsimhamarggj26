ee057e28e23097f49d531a89091e8094d729a09d
Emilio <46302975+BloxRollsDeveloper@users.noreply.github.com>
2026-01-31 00:31:08 +0100

    Navmesh Update
    thanks john unity :D

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,99 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-namespace Enemy
+public class AIController : MonoBehaviour
 {
-   public class EnemyController : MonoBehaviour
-   {
-      [Header("Health")]
-      [SerializeField] private float _health = 100;
+    private GameObject destination;
+    private NavMeshAgent agent;
+    void Start()
+    {
+        destination = GameObject.FindGameObjectWithTag("Player");
 
-      [Header("View")] 
-      [SerializeField] private float _viewCastRadius;
-      [SerializeField] private float _viewRange;
-      private Transform _target;
-      private RaycastHit _hitInfo;
+        agent = GetComponent<NavMeshAgent>();
 
-      [Header("Attack")] [Space(5)]
-      [SerializeField] private GameObject _projectilePrefab;
-      [SerializeField] private Transform _projectileSpawnPoint;
-      [SerializeField] private float _projectileDamage;
-      [SerializeField] private float _projectileSpeed;
-      [SerializeField] private float _projectileRange;
-      [SerializeField] private float _fireRate;
-   
-      private float _fireRateCounter;
+    }
 
-      private void Start() => _target = GameObject.FindWithTag("Player").transform;
-
-      private void Update()
-      {
-         UpdateTargetVisibility();
-      }
-
-      private void UpdateTargetVisibility()
-      {
-         if (_target == null) return;
-         if (Vector3.Distance(_target.position, transform.position) > _viewRange) return;
-         
-         Physics.SphereCast(_projectileSpawnPoint.position, _viewCastRadius, 
-            GetProjectileDirection(), out _hitInfo);
-
-         if (_hitInfo.collider == null) return;
-         if (!_hitInfo.collider.CompareTag("Player")) return;
-
-         UpdateProjectile();
-      }
-   
-      private void UpdateProjectile()
-      {
-         if (_fireRateCounter > Time.time) return;
-
-         var projectileClone = Instantiate(_projectilePrefab, 
-            _projectileSpawnPoint.position, _projectileSpawnPoint.rotation);
-         
-         Destroy(projectileClone, 5f);
-         projectileClone.tag = "EnemyProjectile";
-
-         #region GET COMPONENTS
-         projectileClone.TryGetComponent(out Rigidbody rb); 
-         rb.linearVelocity = GetProjectileDirection() * _projectileSpeed;
-
-         projectileClone.TryGetComponent(out BulletController bulletController);
-         bulletController.projectileDamage = _projectileDamage;
-         #endregion
-         
-         _fireRateCounter = Time.time + _fireRate;
-      }
-   
-      private Vector3 GetProjectileDirection()
-      {
-         return (_target.position - transform.position).normalized;
-      }
-   
-      private void OnTriggerEnter(Collider other)
-      {
-         if (!other.CompareTag("Projectile")) return;
-         
-         other.TryGetComponent(out BulletController bulletController);
-         TakeDamage(bulletController.projectileDamage);
-         
-         Destroy(other.gameObject);
-      }
-   
-      public void TakeDamage(float damage)
-      {
-         _health -= damage;
-         if (_health > 0) return;
-         Destroy(gameObject);
-      }
-      
-      private void OnDrawGizmos()
-      {
-         if (_target == null) return;
-         
-         Gizmos.color = Color.red;
-         Gizmos.DrawWireSphere(_hitInfo.point, _viewCastRadius);
-         Gizmos.DrawRay(_projectileSpawnPoint.position, GetProjectileDirection() * _projectileRange);
-      }
-   }
+    private void Update()
+    {
+        agent.SetDestination(destination.transform.position);
+    }
 }
