2f6d39226a221498d00812cb2d29f0adbb3cca9b
Emilio <46302975+BloxRollsDeveloper@users.noreply.github.com>
2026-01-31 13:53:21 +0100

    a

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -7,6 +7,6 @@ public class SceneChanger : MonoBehaviour
 {
     public void ChangeScene()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("TestScene");
     }
 }
