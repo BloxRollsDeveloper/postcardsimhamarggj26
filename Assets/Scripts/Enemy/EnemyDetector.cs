ffb70258a77732a7603b11bb9a470c4bd4292130
Emilio <46302975+BloxRollsDeveloper@users.noreply.github.com>
2026-02-01 03:05:14 +0100

    pushing tweaks
    Enemy detector has to be looked at

--- Assets/Scripts/Enemy/EnemyDetector.cs
+++ Assets/Scripts/Enemy/EnemyDetector.cs
@@ -1,13 +1,13 @@
-using UnityEngine;
-using SceneManagement;
-public class EnemyDetector : MonoBehaviour
-{
-    private void OnCollisionEnter(Collision col)
-    {
-        if(col.gameObject.tag == "Player")
-        {
-            SceneManager.LoadScene("GameOver");
-        }
-    }
+//using UnityEngine;
+//using SceneManagement;
+//public class EnemyDetector : MonoBehaviour
+//{
+    //private void OnCollisionEnter(Collision col)
+   // {
+        //if(col.gameObject.tag == "Player")
+       // {
+         //   SceneManager.LoadScene("GameOver");
+        //}
+   // }
 
-}
+//}
