b756a8a0cfee2542a9d08586cdfafde25c3c7a32
Emilio <46302975+BloxRollsDeveloper@users.noreply.github.com>
2026-02-01 13:11:41 +0100

    Build setting changes

--- Assets/Scripts/SceneChanges/SceneChanger.cs
+++ Assets/Scripts/SceneChanges/SceneChanger.cs
@@ -9,4 +9,9 @@ public class SceneChanger : MonoBehaviour
     {
         SceneManager.LoadScene("Cutscene");
     }
+
+    public void SkipCutscene()
+    {
+        SceneManager.LoadScene("Sceneworld");
+    }
 }
